6ae20c37744b0cdd2daa6df62ce4cfc1ac54ebae
Fabian Scheler <Fabian.Scheler@gmx.de>
2024-09-09 21:43:21 +0200

    Remove plgx-creation scripts
    As the plugin has a heavy external dependency, it is not suitable for
    plgx-packaging. Instead it should be deployed as dll.

--- make_plgx.cmd
+++
@@ -1,2 +0,0 @@
-@echo on
-call "%CD%\..\bins\KeePass\KeePass.exe" --plgx-create %cd%\SftpSync --plgx-prereq-kp:2.18 --plgx-prereq-net:4.0
--- make_plgx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-"$(pwd)/../bins/KeePass/KeePass.exe" --plgx-create "$(pwd)/SftpSync" --plgx-prereq-kp:2.18 --plgx-prereq-net:4.0
